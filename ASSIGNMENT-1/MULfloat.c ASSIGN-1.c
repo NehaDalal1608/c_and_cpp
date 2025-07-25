5b2949769a031de78d4fe21e88f51800b82caa4a
NehaDalal1608 <nndalal1608@gmail.com>
2025-07-25 22:27:59 +0530

    Add files via upload

---
+++ ASSIGNMENT-1/MULfloat.c ASSIGN-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+
+float num1,num2,multiply;
+
+printf("Enter two numbers=");
+scanf("%f %f",&num1,&num2);
+
+multiply=num1*num2;
+printf("Multiplication of two floating numbers is = %.4f",multiply);
+
+}
---
+++ ASSIGNMENT-1/ascii.c ASSIGN-1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+ char a;
+ 
+ printf("Enter a character=");
+ scanf("%c",&a);
+  
+  printf("The ASCII value of %c=%d",a,a);
+  }
